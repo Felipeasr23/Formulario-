421a9427fda4ca909d0de4d0791988778c80bc71
Felipe Augusto <felipeasr123@gmail.com>
2026-01-30 19:36:28 -0300

    Formulario

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
-const formulario = document.getElementById('formulario');
+const formulario = document.getElementById("formulario");
 
-formulario.addEventListener('submit', function(event){
-    event.preventDefault();
+formulario.addEventListener("submit", function (event) {
+  event.preventDefault();
 
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const mensagem = document.getElementById("msg").value;
 
-    const nome = document.getElementById('nome').value; 
-    const email = document.getElementById('email').value;
-    const mensagem = document.getElementById('msg').value;
+  let erros = [];
 
-    let erros = [];
+  if (nome.trim() === "") {
+    erros.push("O nome é obrigatório!");
+  }
 
-    if(nome.trim() === ''){
-        erros.push('O nome é obrigatório!');
-    }
+  const emailRegex = /^[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
-    const emailRegex =  /^[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  if (!emailRegex.test(email)) {
+    erros.push("Por favor, insira um e-mail válido.");
+  }
 
-    if(!emailRegex.test(email)){
-        erros.push('Por favor, insira um e-mail válido.');
-    }
+  if (mensagem.trim().length < 10) {
+    erros.push("A mensagem deve ter pelo menos 10 caracteres!");
+  }
 
-    if(mensagem.trim().length < 10){
-        erros.push('A mensagem deve ter pelo menos 10 caracteres!');
-    }
+  const erroDiv = document.getElementById("erro");
 
-    const erroDiv = document.getElementById('erro');
-
-    if(erros.length > 0){
-        erroDiv.innerHTML = erros.join('<br>');
-        erroDiv.style.color = 'red';
-    } else{
-        erroDiv.innerHTML = 'Formulário enviado com sucesso!';
-        erroDiv.style.color = 'green';
-        formulario.reset();
-    }
-
-})
+  if (erros.length > 0) {
+    erroDiv.innerHTML = erros.join("<br>");
+    erroDiv.style.color = "red";
+  } else {
+    erroDiv.innerHTML = "Formulário enviado com sucesso!";
+    erroDiv.style.color = "green";
+    formulario.reset();
+  }
+});
